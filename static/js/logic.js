26233340ecc8263d146121672a6a7d7f915b37d1
ANDREW KRIEGER <ajkriegz@gmail.com>
2023-08-22 20:39:56 -0400

    updating logic to log entries in console

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,21 +1,44 @@
-// Connect to Flask API for data using D3.json
-let url = '/data';
-// function init() ???
-// var url = '/';
-
-d3.json('http://127.0.0.1:5000/data').then( data => {
-    console.log(data)
-});
-
+// // Check status of connection
 // const dataPromise = d3.json(url);
 // console.log("Data Promise: ", dataPromise);
 
-// // Check to see if you get a 200 or 404
 // const myRequest = new Request(url);
-
 // fetch(myRequest).then((response) => {
 //   console.log(response.status); //returns 200
-  
 // });
 
-// console.log(data[0])
+// Connect to Flask API for data using D3.json
+var url = '/data';
+var beer = {}
+
+let showObj = function() {
+    for (let prop in beer) {
+        // console.log(prop); // data
+        // console.log(beer[prop]); // (3197) [Array(2), Array(2), Array(2)
+        console.log(beer[prop][0]); // ['Brown Ale', 'Alaskan Brewing Co. Alaskan Amber']
+        // log every single beer
+        for (let i = 0; i < beer[prop].length; i++) {
+            console.log(beer[prop][i][0]) // This counts the styles in the 0 index. Remove [0] to get every beer pair.
+        }
+    };
+}
+
+d3.json('http://127.0.0.1:5000/data')
+.then( data => {
+    console.log(data); // {data: Array(3197)}
+    beer = data;
+    console.log(beer) // {data: Array(3197)}
+    showObj();
+});
+
+// fetch also works
+// fetch('http://127.0.0.1:5000/data')
+//     .then( resp => {
+//         return resp.json();
+//     })
+//     .then( data => {
+//         console.log(data);
+//         beer = data
+//         console.log(beer)
+//         showObj();
+//     });
